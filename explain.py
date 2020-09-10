7440bfc6bfa944fd8fad52fdc7523431ebd5d170
PopckovS <popckovM5@yandex.com>
2020-09-10 17:15:06 +0300

    Добавил автоматическую генерауию множества процессов, по процессу для бота.

--- explain.py
+++ explain.py
@@ -1,38 +1,38 @@
-"""
-Дополнительная Информация:
+"""Установка PyMySQL для работы с БД"""
+     # pip3 install PyMySQL
 
-Установка localtunnel для связки вебхуком с FaceBook мессекнджером:
-    sudo apt install nodejs -y
-    sudo apt install npm -y
 
-    node -v
-    npm -v
+"""Установка localtunnel для связки вебхуком с FaceBook мессекнджером:"""
+    # sudo apt install nodejs -y
+    # sudo apt install npm -y
+    #
+    # node -v
+    # npm -v
+    #
+    # npm install -g localtunnel
+    #
+    # lt -h http://serverless.social --subdomain sergio-fb-bot -p 5000
 
-    npm install -g localtunnel
-
-    lt -h http://serverless.social --subdomain sergio-fb-bot -p 5000
-
-Дефолтный SSL сертификат для Flask устанавливается модулем.
+"""Дефолтный SSL сертификат для Flask устанавливается модулем."""
     # pip3 install flask-sslify
-    from flask_sslify import SSLify
-    sslify = SSLify(app)
+    # from flask_sslify import SSLify
+    # sslify = SSLify(app)
 
-Установка emoji для телеграмма
-    pip3 install emoji
-    from emoji import emojize
+"""Установка emoji для телеграмма"""
+    # pip3 install emoji
+    # from emoji import emojize
 
-Установка Библиотеки telebot для Телеграм Бота
-    pip3 install pytelegrambotapi
-    import telebot
+"""Установка Библиотеки telebot для Телеграм Бота"""
+    # pip3 install pytelegrambotapi
+    # import telebot
 
-Посмотреть Доступные пути, для импортирования модулей
-    import sys
-    print('\n\nПеременная PYTHONPATH содержит', sys.path, '\n')
+"""Посмотреть Доступные пути, для импортирования модулей"""
+    # import sys
+    # print('\n\nПеременная PYTHONPATH содержит', sys.path, '\n')
 
-# Красивое форматирование Даты времени
+"""Красивое форматирование Даты времени"""
 # dt = datetime.fromtimestamp(message['time'])
 # dt_beauty = dt.strftime('%Y:%m:%d %H:%M:%S')
 
-"""
 
 
